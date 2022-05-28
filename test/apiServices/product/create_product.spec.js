a5076f6e148657be13e056122bba2182815fbc7d
EzequielK-source <programacionezequielk@gmail.com>
2022-05-28 00:19:53 -0300

    (test) add missing any CreateProduct param throw error clausule

--- test/apiServices/product/create_product.spec.js
+++ test/apiServices/product/create_product.spec.js
@@ -1,7 +1,7 @@
 //Test deps and sets
 const chai = require('chai'),
     chaiAsPromised = require('chai-as-promised')
-	deepEqualInAnyOrder = require('deep-equal-in-any-order');
+deepEqualInAnyOrder = require('deep-equal-in-any-order');
 chai.use(chaiAsPromised);
 chai.use(deepEqualInAnyOrder)
 const {
@@ -19,9 +19,9 @@ const {
 //Domain imports
 const CreateProduct = require('src/apiServices/product/create_product');
 describe('CreateProduct test', () => {
-	before(async()=>{
-		await sequelize.query('DELETE FROM products');
-	})
+    before(async () => {
+        await sequelize.query('DELETE FROM products');
+    })
     describe('basic product create', () => {
         /**
          * exceute CreateProduct function and
@@ -33,8 +33,8 @@ describe('CreateProduct test', () => {
             name: "aProductName",
             description: "a product description",
             photo: "path/to/product/photo",
-			price: 10
-        }
+            price: 10
+        };
         it('create product', () => {
             expect(CreateProduct(productParams))
                 .to
@@ -43,12 +43,76 @@ describe('CreateProduct test', () => {
                 .not
                 .rejected;
         });
-        it('product persisted', async() => {
-			const [product] = await sequelize.query(`SELECT * FROM products WHERE barcode='${productParams.barcode}'`, {
-				type: QueryTypes.SELECT
-			})
-			expect(product).to.be.not.undefined;
-			expect(product).to.equalInAnyOrder(productParams);
+        it('product persisted', async () => {
+            const [product] = await sequelize.query(`SELECT * FROM products WHERE barcode='${productParams.barcode}'`, {
+                type: QueryTypes.SELECT
+            })
+            expect(product).to.be.not.undefined;
+            expect(product).to.equalInAnyOrder(productParams);
+        });
+    });
+    describe('CreateProduct error clausules', () => {
+        describe('missing any CreateProduct param throw error', () => {
+            it('missing barcode param ', () => {
+                expect(CreateProduct({
+                        name: 'aName',
+                        description: 'a product description',
+                        photo: 'a/product/photo/path',
+                        price: 10
+                    }))
+                    .to
+                    .eventually
+                    .be
+                    .rejected;
+            });
+			it('missing name param ', () => {
+                expect(CreateProduct({
+                        barcode: 'aBarcode',
+                        description: 'a product description',
+                        photo: 'a/product/photo/path',
+                        price: 10
+                    }))
+                    .to
+                    .eventually
+                    .be
+                    .rejected;
+            });
+			it('missing description param ', () => {
+                expect(CreateProduct({
+                        barcode: 'aBarcode',
+                        name: 'aName',
+                        photo: 'a/product/photo/path',
+                        price: 10
+                    }))
+                    .to
+                    .eventually
+                    .be
+                    .rejected;
+            });
+			it('missing photo param ', () => {
+                expect(CreateProduct({
+                        barcode: 'aBarcode',
+                        name: 'aName',
+                        description: 'a product description',
+                        price: 10
+                    }))
+                    .to
+                    .eventually
+                    .be
+                    .rejected;
+            });
+			it('missing price param ', () => {
+                expect(CreateProduct({
+                        barcode: 'aBarcode',
+                        name: 'aName',
+                        description: 'a product description',
+                        photo: 'a/product/photo/path'
+                    }))
+                    .to
+                    .eventually
+                    .be
+                    .rejected;
+            });
         });
     });
 });

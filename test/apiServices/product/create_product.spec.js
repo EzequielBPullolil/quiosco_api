0b8b07a4bc3b0f70be9bdf51a1922456714f6fbb
EzequielK-source <programacionezequielk@gmail.com>
2022-05-28 00:05:32 -0300

    (test) Create basic product create test and persist test

--- test/apiServices/product/create_product.spec.js
+++ test/apiServices/product/create_product.spec.js
@@ -1,18 +1,51 @@
 //Test deps and sets
 const chai = require('chai'),
-	chaiAsPromised = require('chai-as-promised');
+    chaiAsPromised = require('chai-as-promised')
+	deepEqualInAnyOrder = require('deep-equal-in-any-order');
 chai.use(chaiAsPromised);
+chai.use(deepEqualInAnyOrder)
+const {
+    expect,
+    assert
+} = chai;
+//app imports
+const {
+    QueryTypes
+} = require('sequelize');
+const {
+    sequelize
+} = require('src/services/sequelize/index');
 
-const {expect, assert} = chai;
 //Domain imports
 const CreateProduct = require('src/apiServices/product/create_product');
 describe('CreateProduct test', () => {
-	describe('basic product create', () => {
-		it('create product', () => {
-
-		});
-		it('product persisted', () => {
-
-		});
-	});
+    describe('basic product create', () => {
+        /**
+         * exceute CreateProduct function and
+         * verify if they dont throw error and
+         * if product with same barcode are persisted
+         **/
+        const productParams = {
+            barcode: "aBarcode",
+            name: "aProductName",
+            description: "a product description",
+            photo: "path/to/product/photo",
+			price: 10
+        }
+        it('create product', () => {
+            expect(CreateProduct(productParams))
+                .to
+                .eventually
+                .be
+                .not
+                .rejected;
+        });
+        it('product persisted', async() => {
+			const [product] = await sequelize.query(`SELECT * FROM products WHERE barcode='${productParams.barcode}'`, {
+				type: QueryTypes.SELECT
+			})
+			expect(product).to.be.not.undefined;
+			expect(product).to.equalInAnyOrder(productParams);
+        });
+    });
 });

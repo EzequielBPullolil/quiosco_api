c8013997093fcd9da2470b40fbc80a7f3670166b
EzequielK-source <programacionezequielk@gmail.com>
2022-05-29 20:33:54 -0300

    (feat) Add post method to products/ route

--- src/routes/product_route.js
+++ src/routes/product_route.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const productRouter = express.Router();
-
+const ProductCreator = require('src/apiServices/product/create_product')
 productRouter.route('/')
 	.get((req,res)=>{
 		return res.sendStatus(200)
+	})
+	.post(async (req,res)=>{
+		return res.sendStatus(201)
 	})
 
 module.exports = productRouter;

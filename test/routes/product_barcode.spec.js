28490a396999482f477295ad79e48949bef12c15
EzequielK-source <programacionezequielk@gmail.com>
2022-07-18 21:24:03 -0300

    (Test) Add product/barcode put test

--- test/routes/product_barcode.spec.js
+++ test/routes/product_barcode.spec.js
@@ -1,8 +1,9 @@
 //Test deps and sets
 const chai = require('chai'),
     chaiHttp = require('chai-http')
+    deepEqualInAnyOrder = require('deep-equal-in-any-order');
 chai.use(chaiHttp);
-
+chai.use(deepEqualInAnyOrder)
 const {
     expect,
     assert,
@@ -15,13 +16,13 @@ const app = require('src/app');
 
 describe('ProductRouter barcode route test', () => {
     let productTestSuject;
-    before(async() => {
+    before(async () => {
         productTestSuject = {
-            barcode:'ProductBarcodeRouterTestProduct',
-            name:'a name for test',
-            price:1000,
-            description:'a product description',
-            photo:'/path/to/photo'
+            barcode: 'ProductBarcodeRouterTestProduct',
+            name: 'a name for test',
+            price: 1000,
+            description: 'a product description',
+            photo: '/path/to/photo'
         }
         await CreateTestProduct(productTestSuject)
     });
@@ -36,6 +37,35 @@ describe('ProductRouter barcode route test', () => {
                 done()
             })
     });
+    it('barcode put', (done) => {
+        /**
+         * Verify /products/barcode/ endpoint with put method
+         */
+        const fieldsForUpdate = {
+            name: 'abc'
+        }
+        request(app)
+            .put(`/products/barcode/${productTestSuject.barcode}`)
+            .send(fieldsForUpdate)
+            .end((err, res) => {
+                if (err) done(err);
+
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.deep.equal({
+                    status:'product updated',
+                    product:{
+                        barcode: 'ProductBarcodeRouterTestProduct',
+                        name: fieldsForUpdate.name,
+                        price: 1000,
+                        description: 'a product description',
+                        photo: '/path/to/photo'
+                    }
+                })
+
+                done();
+            })
+    });
     it('barcode delete', (done) => {
         request(app)
             .delete(`/products/barcode/${productTestSuject.barcode}`)
@@ -45,7 +75,7 @@ describe('ProductRouter barcode route test', () => {
                 expect(res).to.have.status(200);
                 expect(res).to.be.json;
                 expect(res.body).to.be.deep.equal({
-                    status:'product deleted'
+                    status: 'product deleted'
                 });
                 done();
             })

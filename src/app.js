0879dcd9324fb1462c51b69f40fd79c33d5f4e13
EzequielK-source <programacionezequielk@gmail.com>
2022-07-19 21:10:12 -0300

    (Feature) UserRouter linked to express app

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const app = express();
 app.use(express.json());
 //Domain
 const productRouter = require('src/routes/product_route');
+const userRouter = require('src/routes/user_route');
 
 app.use('/products', productRouter);
+app.use('/users', userRouter);
 app.get('/', (req, res) => {
     return res.sendStatus(200)
 })

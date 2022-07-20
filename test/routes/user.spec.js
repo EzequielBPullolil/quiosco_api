1fc673502827294c6f226437a707f01c20701e5c
EzequielK-source <programacionezequielk@gmail.com>
2022-07-19 21:31:01 -0300

    (Test) Create UserRouter test

--- test/routes/user.spec.js
+++ test/routes/user.spec.js
@@ -17,7 +17,33 @@ describe('UserRouter test', () => {
             .end((err,res)=>{
                 if(err) done (err);
 
-                expect(res).to.have.status(200)
+                expect(res).to.have.status(200);
+                done();
+            })
+    });
+
+    it('UserRouter post test', (done) => {
+        /**
+         * Request /users post endpoint and 
+         * verify response
+         */
+        const userParams = {
+            name:'AuserName',
+            password:'ApasswordForUser',
+            rol: 0
+        }
+        request(app)
+            .post('/users')
+            .send(userParams)
+            .end((err,res)=>{
+                if(err) done(err);
+
+                expect(res).to.have.status(201);
+                expect(res).to.be.json;
+                expect(res.body.status).to.be.equal('User registered')
+                expect(res.body.user.name).to.be.equal(userParams.name)
+                expect(res.body.user.password).to.be.not.equal(userParams.password)
+
                 done();
             })
     });

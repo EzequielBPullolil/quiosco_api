834db83a2a0917dde159f00e870c44e54d198e12
EzequielK-source <programacionezequielk@gmail.com>
2022-07-19 21:31:27 -0300

    (Feature) Create post route for UserRouter

--- src/routes/user_route.js
+++ src/routes/user_route.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const userRouter = express.Router();
-
+const CreateUser = require('src/apiServices/user/create_user')
 userRouter.route('/')
     .get((req,res)=>{
         return res.sendStatus(200)
+    })
+    .post(async(req,res)=>{
+        const user = await CreateUser(req.body);
+        return res.status(201).json({
+            status:'User registered',
+            user: user
+        });
     })
 
 
